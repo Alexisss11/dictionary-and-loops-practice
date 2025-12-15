3ed676beea24cc480eae80eac6d4cf647dcff838
Alexisss11 <alexispurple1978@gmail.com>
2025-12-15 20:50:13 +0000

    practice

--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -43,19 +43,33 @@ import student_data
 
 # print(student_data.students)
 students = student_data.students
-print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(len(students))
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+# print(students[0]['LName'])
+# print(students[0]['FName'])
+# print(students[0]['GL'])
 
+# print("--------------------------")
+# print(students[1]['CPSID'])
+# print(students[1]['Email'][0])
+# print(students[1]['GL'])
+# print(students[1]['HR'])
+# print("--------------------------")
+# print(students[35]['CPSID'])
+# print(students[35]['Email'][1])
+# print(students[35]['GL'])
+# print(students[35]['HR'])
+# print("--------------------------")
 # What is being counted?
 
 # If the output is 36, what does that tell you?
-
+# the amount of students in the list
 # If one new student enrolls, what happens to this number?
-
+#  it goes up to 37
 # Why would a school use this in a real attendance system?
-
+# To keep track fo everyones information 
 
 
 
@@ -68,11 +82,24 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     print("_"*25)
+
+# for student in students:
+#     print(student['HR'])
+#     print(student['GL'])
+#     print(student['CPSID'])
+#     if student['GL'] >= 10:
+#         print("You have off campus")
+#     if student['LName'][0] == "R":
+#         print(student['Combo,Name'])
+#         print(student['HR'])
+#     if student['LName'][0] == "L":
+#         break
+#     print("_"*25)
 
 # What does the loop variable student represent on each pass?
 
@@ -95,9 +122,9 @@ for student in students:
         print("this works")
 
 # What is the goal of this search?
-
+#  to see if youyr name is in the system
 # What happens if the name is not found?
-
+# it wont  print anything
 # Why is the comparison happening inside the loop?
 
 # What would happen if two students had the same name?
